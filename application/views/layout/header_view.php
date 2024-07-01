97d9535c34d831c3310a93fb75d7c6d3dbe011c8
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-01 16:22:31 +0300

    Add header_view for the layout

---
+++ application/views/layout/header_view.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <div>
+        <ul>
+            <li><a href="/">Главная</a></li>
+            <li><a href="/portfolio">Портфолио</a></li>
+            <li><a href="/about">Обо мне</a></li>
+            <li><a href="/contacts">Контакты</a></li>
+            <li><a href="/time">Точное время</a></li>
+        </ul>
+    </div>
+</header>
